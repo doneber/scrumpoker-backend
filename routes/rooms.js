1556201e191850c206bcfcbaa5abb1631ec30839
doneber <doneber98@gmail.com>
2021-02-09 22:59:27 -0400

    Add firestore database

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,111 +1,130 @@
 const express = require('express');
 var router = express.Router();
-const db = require('../db.json')
+const dbFake = require('../db.json')
+const serviceAccount = require('./../services/scrum poker 5000-f4c108c23730.json');
 
-function findIndexById(id) {
-  for (let index = 0; index < db.rooms.length; index++)
-    if (db.rooms[index].id == id)
-      return index
-  return -1
+var admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+
+async function getRooms (req, res){
+  const allRooms = []
+  const snapshot = await db.collection('rooms').get();
+  snapshot.forEach((doc) => {
+    allRooms.push({id:doc.id, ...doc.data()})
+  });
+
+  res.json(allRooms)
 }
-function getRooms (req, res){
-  res.json(db.rooms)
-}
-function getRoomById (req, res){
-  const index = findIndexById(req.params.roomId)
-  index >= 0?res.json(db.rooms[index]):res.send('Room not found')
-}
-function postRoom (req, res){
-  const newRoom = {id: new Date().getTime(), name: req.body.nameRoom, users: []}
-  db.rooms.push(newRoom)  
-  res.json(newRoom)
-}
-function putRoom (req, res){
-  const index = findIndexById(req.params.roomId)
-  if (index >= 0){
-    db.rooms[index].name = req.body.name?req.body.name:db.rooms[index].name
-    res.json(db.rooms[index])
-  }
+async function getRoomById (req, res){
+  const roomRef = await db.collection('rooms').doc(req.params.roomId).get()
+  const room = await roomRef.data()
+  if (room)
+    res.json({id:req.params.roomId, room})
   else
-    res.send('Room not found')
+    res.send('There was a problem')
 }
-function deleteRoom (req, res){
-  const index = findIndexById(req.params.roomId)
-  if (index >= 0) {
-    const Room = db.rooms[index]
-    db.rooms.splice(index,1)
-    res.json(Room)
-  }
-  else
-    res.send('Room not found')
+async function postRoom (req, res){
+  // The body must be like : { name: "user name" }
+  const newRoom = { name: req.body.name }
+  let respuesta = {}
+  const room = await db.collection('rooms').add(newRoom)
+  .then(function (docRef) {
+    respuesta = {id: docRef.id, ...newRoom}
+    return docRef
+  })
+  res.json(respuesta)
 }
-function userRoomById(req, res) {
-  const index = findIndexById(req.params.roomId)
-  let indexUser = -1
-  if(index >= 0)
-    for (let i = 0; i < db.rooms[index].users.length; i++)
-      if (db.rooms[index].users[i].id == req.params.userId) {
-        indexUser = i
-        break
-      }
-  indexUser >= 0? res.json(db.rooms[index].users[indexUser]) : res.send('Room not found')
+// function putRoom (req, res){
+//   const index = findIndexById(req.params.roomId)
+//   if (index >= 0){
+//     dbFake.rooms[index].name = req.body.name?req.body.name:dbFake.rooms[index].name
+//     res.json(dbFake.rooms[index])
+//   }
+//   else
+//     res.send('Room not found')
+// }
+// function deleteRoom (req, res){
+//   const index = findIndexById(req.params.roomId)
+//   if (index >= 0) {
+//     const Room = dbFake.rooms[index]
+//     dbFake.rooms.splice(index,1)
+//     res.json(Room)
+//   }
+//   else
+//     res.send('Room not found')
+// }
+// function userRoomById(req, res) {
+//   const index = findIndexById(req.params.roomId)
+//   let indexUser = -1
+//   if(index >= 0)
+//     for (let i = 0; i < dbFake.rooms[index].users.length; i++)
+//       if (dbFake.rooms[index].users[i].id == req.params.userId) {
+//         indexUser = i
+//         break
+//       }
+//   indexUser >= 0? res.json(dbFake.rooms[index].users[indexUser]) : res.send('Room not found')
+// }
+async function usersInRoom(req, res) {
+  const allUsers = []
+  const snapshot = await db.collection('rooms').doc(req.params.roomId).collection('users').get()
+  snapshot.forEach(doc => {
+    allUsers.push({id:doc.id, ...doc.data()})
+  });
+  res.json(allUsers)
 }
-function usersRoom(req, res) {
-  const index = findIndexById(req.params.roomId)
-  if(index >= 0){
-    res.json(db.rooms[index].users)
-  } else res.send('Room not found')
+async function createUserInRoom (req, res){
+  // The body must be like : { name: "user name" }
+  const newUser = { name: req.body.name, value: -1 }
+  let respuesta = {}
+  const room = await db.collection('rooms').doc(req.params.roomId).collection('users').add(newUser)
+  .then(function (docRef) {
+    respuesta = {id: docRef.id, ...newUser}
+    return docRef
+  })
+  res.json(respuesta)
 }
-function createUserRoom (req, res){
-  const index = findIndexById(req.params.roomId)
-  if(index >= 0){
-    const newUser = {id: new Date().getTime(), name: req.body.name, value: req.body.value }
-    db.rooms[index].users.push(newUser)
-    res.json(newUser)
-  } else res.send('Room not found')
-  
-  const newRoom = {id: new Date().getTime(), name: req.body.name}
-  res.json(newRoom)
+async function patchUserRoom(req, res) {
+  // The body must be like : { name: "user name" }
+  const updateUser = { value: req.body.value }
+  let respuesta = {}
+  const room = await db.collection('rooms').doc(req.params.roomId).collection('users').doc(req.params.userId).update(updateUser)
+  .then(function (docRef) {
+    respuesta = {id: docRef.id, ...updateUser}
+    return docRef
+  })
+  res.json(respuesta)
 }
-function patchUserRoom(req, res) {
-  const index = findIndexById(req.params.roomId)
-  let indexUser = -1
-  if(index >= 0)
-    for (let i = 0; i < db.rooms[index].users.length; i++)
-      if (db.rooms[index].users[i].id == req.params.userId) {
-        indexUser = i
-        db.rooms[index].users[i].value = req.body.value
-        break
-      }
-  indexUser >= 0? res.json(db.rooms[index].users[indexUser]) : res.send('Room not found')
-}
-function deleteUserRoom(req, res) {
-  const index = findIndexById(req.params.roomId)
-  let indexUser = -1
-  if(index >= 0)
-    for (let i = 0; i < db.rooms[index].users.length; i++)
-      if (db.rooms[index].users[i].id == req.params.userId) {
-        indexUser = i
-        break
-      }
-  if (indexUser >= 0) {
-    const userDeleted = db.rooms[index].users[indexUser]
-    db.rooms[index].users.splice(indexUser,1)
-    res.json(userDeleted)
-  }
-  else
-    res.send('User not found')
-}
+// function deleteUserRoom(req, res) {
+//   const index = findIndexById(req.params.roomId)
+//   let indexUser = -1
+//   if(index >= 0)
+//     for (let i = 0; i < dbFake.rooms[index].users.length; i++)
+//       if (dbFake.rooms[index].users[i].id == req.params.userId) {
+//         indexUser = i
+//         break
+//       }
+//   if (indexUser >= 0) {
+//     const userDeleted = dbFake.rooms[index].users[indexUser]
+//     dbFake.rooms[index].users.splice(indexUser,1)
+//     res.json(userDeleted)
+//   }
+//   else
+//     res.send('User not found')
+// }
 // Get all, Get one, create, Update, delete
 router.get("/", getRooms)
 router.get("/:roomId", getRoomById)
 router.post('', postRoom)
-router.put('/:roomId', putRoom)
-router.delete('/:roomId', deleteRoom)
+// router.put('/:roomId', putRoom)
+// router.delete('/:roomId', deleteRoom)
 // Users:
-router.get('/:roomId/users', usersRoom)
-router.get('/:roomId/users/:userId', userRoomById)
-router.post('/:roomId/users', createUserRoom)
+router.get('/:roomId/users', usersInRoom)
+// router.get('/:roomId/users/:userId', userRoomById)
+router.post('/:roomId/users', createUserInRoom)
 router.patch('/:roomId/users/:userId', patchUserRoom)
-router.delete('/:roomId/users/:userId', deleteUserRoom)
+// router.delete('/:roomId/users/:userId', deleteUserRoom)
 module.exports = router
